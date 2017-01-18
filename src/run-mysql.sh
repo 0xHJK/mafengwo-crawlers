7aaa284e6e729a3623f9e38e178b295c26ed728c
0xHJK <HJKdev+GitHub@gmail.com>
2017-01-18 22:47:08 +0800

    run mysql docker

---
+++ src/run-mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO='mysql'
+NAME='localmysql'
+PASSWORD='root'
+DATABASE='ayou'
+
+run(){
+    if [[ `docker ps -a | awk '{print $NF}' | grep "${NAME}"` ]]; then
+        docker start ${NAME}
+    else
+        docker run -d --name ${NAME} -e MYSQL_ROOT_PASSWORD=${PASSWORD} -e MYSQL_DATABASE=${DATABASE} -p 3306:3306 ${REPO}
+}
