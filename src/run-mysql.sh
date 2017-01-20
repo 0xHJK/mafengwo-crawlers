61ddf71a1238e6848802ab89d00e54f20c529476
0xHJK <HJKdev+GitHub@gmail.com>
2017-01-20 21:20:38 +0800

    charset utf8mb4

--- src/run-mysql.sh
+++ src/run-mysql.sh
@@ -9,7 +9,7 @@ run(){
     if [[ `docker ps -a | awk '{print $NF}' | grep "${NAME}"` ]]; then
         docker start ${NAME}
     else
-        docker run -d --name ${NAME} -e MYSQL_ROOT_PASSWORD=${PASSWORD} -e MYSQL_DATABASE=${DATABASE} -p 3306:3306 ${REPO}
+        docker run -d --name ${NAME} -e MYSQL_ROOT_PASSWORD=${PASSWORD} -e MYSQL_DATABASE=${DATABASE} -p 3306:3306 ${REPO} --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
     fi
 }
 
